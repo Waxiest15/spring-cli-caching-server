4ea7bdde4dac8d8a4b7c2c05aa030ab77ad9a2d5
waxiest15 <waxiest15@gmail.com>
2026-06-23 12:33:55 -0600

    [update]: @EnableCaching to allow caching to occur on the app

--- src/main/java/com/waxiest/spring_cli_caching_server/SpringCliCachingServerApplication.java
+++ src/main/java/com/waxiest/spring_cli_caching_server/SpringCliCachingServerApplication.java
@@ -2,8 +2,10 @@ package com.waxiest.spring_cli_caching_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringCliCachingServerApplication {
 
 	public static void main(String[] args) {
